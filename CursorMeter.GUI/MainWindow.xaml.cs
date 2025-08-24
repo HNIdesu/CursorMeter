5f50c377322482460f227503eef5d215d28641ae
hnidesu <hnidesu@outlook.com>
2025-08-24 16:27:56 +0800

    Initial commit.

---
+++ CursorMeter.GUI/MainWindow.xaml.cs
@@ -0,0 +1,169 @@
+﻿using CursorMeter.GUI.Properties;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Threading;
+namespace CursorMeter.GUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly DispatcherTimer mTimer;
+        private Point? mPrevPosition = null;
+        private int mInterval = 50;
+        private static nint mHookId = 0;
+        private static bool mIsMouseLeftButtonPressed = false;
+        private static bool mIsMouseRightButtonPressed = false;
+        private bool mHoldToMeasure = false;
+
+        private static nint MouseHookCallback(int nCode, nint wParam, nint lParam)
+        {
+            if (nCode >= 0)
+            {
+                var hookStruct = Marshal.PtrToStructure<WinAPI.MSLLHOOKSTRUCT>(lParam);
+                switch ((int)wParam)
+                {
+                    case WinAPI.WM_LBUTTONDOWN:
+                        mIsMouseLeftButtonPressed = true;
+                        break;
+                    case WinAPI.WM_LBUTTONUP:
+                        mIsMouseLeftButtonPressed = false;
+                        break;
+                    case WinAPI.WM_RBUTTONDOWN:
+                        mIsMouseRightButtonPressed = true;
+                        break;
+                    case WinAPI.WM_RBUTTONUP:
+                        mIsMouseRightButtonPressed = false;
+                        break;
+                }
+            }
+
+            return WinAPI.CallNextHookEx(mHookId, nCode, wParam, lParam);
+        }
+
+        private void RemoveTopMost()
+        {
+            var hWnd = new WindowInteropHelper(this).Handle;
+            WinAPI.SetWindowPos(hWnd, WinAPI.HWND_NOTOPMOST, 0, 0, 0, 0, WinAPI.SWP_NOMOVE | WinAPI.SWP_NOSIZE | WinAPI.SWP_SHOWWINDOW);
+        }
+        private void MakeTopMost()
+        {
+            var hWnd = new WindowInteropHelper(this).Handle;
+            WinAPI.SetWindowPos(hWnd, WinAPI.HWND_TOPMOST, 0, 0, 0, 0, WinAPI.SWP_NOMOVE | WinAPI.SWP_NOSIZE | WinAPI.SWP_SHOWWINDOW);
+        }
+
+        private void StartCursorMeter()
+        {
+            mTimer.Start();
+            mHookId = WinAPI.SetWindowsHookEx(WinAPI.WH_MOUSE_LL, MouseHookCallback, 0, 0);
+            CursorMeterSwitch.Content = "Stop";
+        }
+
+        private void StopCursorMeter()
+        {
+            mTimer.Stop();
+            WinAPI.UnhookWindowsHookEx(mHookId);
+            CursorMeterSwitch.Content = "Start";
+        }
+        private void LoadSettings()
+        {
+            AlwaysOnTopCheck.IsChecked = Settings.Default.AlwaysOnTop;
+            HoldToMeasureCheck.IsChecked = Settings.Default.HoldToMeasure;
+            DeltaTime.Text = Settings.Default.DeltaTime.ToString();
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+            CursorMeterSwitch.Checked += (sender, ev) =>
+            {
+                StartCursorMeter();
+            };
+            CursorMeterSwitch.Unchecked += (sender, ev) =>
+            {
+                StopCursorMeter();
+            };
+            HoldToMeasureCheck.Checked += (sender, ev) =>
+            {
+                mHoldToMeasure = true;
+                if (Settings.Default.HoldToMeasure != mHoldToMeasure)
+                {
+                    Settings.Default.HoldToMeasure = mHoldToMeasure;
+                    Settings.Default.Save();
+                }
+            };
+            HoldToMeasureCheck.Unchecked += (sender, ev) =>
+            {
+                mHoldToMeasure = false;
+                if (Settings.Default.HoldToMeasure != mHoldToMeasure)
+                {
+                    Settings.Default.HoldToMeasure = mHoldToMeasure;
+                    Settings.Default.Save();
+                }
+            };
+            AlwaysOnTopCheck.Checked += (sender, ev) =>
+            {
+                MakeTopMost();
+                if (!Settings.Default.AlwaysOnTop)
+                {
+                    Settings.Default.AlwaysOnTop = true;
+                    Settings.Default.Save();
+                }
+            };
+            AlwaysOnTopCheck.Unchecked += (sender, ev) =>
+            {
+                RemoveTopMost();
+                if (Settings.Default.AlwaysOnTop)
+                {
+                    Settings.Default.AlwaysOnTop = false;
+                    Settings.Default.Save();
+                }
+            };
+            DeltaTime.TextChanged += (sender, ev) =>
+            {
+                mInterval = int.Parse(DeltaTime.Text);
+                if (mInterval != Settings.Default.DeltaTime)
+                {
+                    Settings.Default.DeltaTime = mInterval;
+                    Settings.Default.Save();
+                }
+            };
+            Loaded += (sender, ev) =>
+            {
+                if (Settings.Default.AlwaysOnTop)
+                    MakeTopMost();
+            };
+            LoadSettings();
+            mTimer = new DispatcherTimer(
+                new TimeSpan(0, 0, 0, 0, mInterval),
+                DispatcherPriority.Input,
+                (sender, ev) =>
+                {
+                    if ((!mHoldToMeasure) || mIsMouseLeftButtonPressed || mIsMouseRightButtonPressed)
+                    {
+                        WinAPI.GetCursorPos(out var rawPosition);
+                        var position = new Point(rawPosition.X, rawPosition.Y);
+                        if (mPrevPosition == null)
+                            mPrevPosition = position;
+                        else
+                        {
+                            var distance = Point.Subtract(position, mPrevPosition.Value).Length;
+                            var speed = distance / mInterval * 1000;
+                            SpeedText.Text = $"Speed: {speed:F2} px/s";
+                            mPrevPosition = position;
+                        }
+                    }
+                    else
+                    {
+                        SpeedText.Text = "Speed: 0.00 px/s";
+                    }
+                },
+                Dispatcher.CurrentDispatcher);
+            mTimer.Stop();
+        }
+
+    }
+}
---
+++ CursorMeter.GUI/WinAPI.cs
@@ -0,0 +1,53 @@
+﻿using System.Runtime.InteropServices;
+
+namespace CursorMeter.GUI
+{
+    internal static class WinAPI
+    {
+        public struct POINT
+        {
+            public int X;
+            public int Y;
+        }
+        [StructLayout(LayoutKind.Sequential)]
+        public struct MSLLHOOKSTRUCT
+        {
+            public POINT pt;
+            public uint mouseData;
+            public uint flags;
+            public uint time;
+            public nint dwExtraInfo;
+        }
+        public const int WH_MOUSE_LL = 14;
+        public const int WM_MOUSEMOVE = 0x0200;
+        public const int WM_LBUTTONDOWN = 0x0201;
+        public const int WM_LBUTTONUP = 0x0202;
+        public const int WM_RBUTTONDOWN = 0x0204;
+        public const int WM_RBUTTONUP = 0x0205;
+        public const int WM_MOUSEWHEEL = 0x020A;
+        public const nint HWND_TOPMOST = -1;
+        public const uint SWP_NOMOVE = 0x0002;
+        public const uint SWP_NOSIZE = 0x0001;
+        public const uint SWP_SHOWWINDOW = 0x0040;
+        public const nint HWND_NOTOPMOST = -2;
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool GetCursorPos(out POINT lpPoint);
+        public delegate nint LowLevelMouseProc(int nCode, nint wParam, nint lParam);
+        [DllImport("user32.dll")]
+        public static extern nint SetWindowsHookEx(int idHook, LowLevelMouseProc lpfn, nint hMod, uint dwThreadId);
+
+        [DllImport("user32.dll")]
+        public static extern bool UnhookWindowsHookEx(nint hhk);
+
+        [DllImport("user32.dll")]
+        public static extern nint CallNextHookEx(nint hhk,
+            int nCode, nint wParam, nint lParam);
+
+        [DllImport("kernel32.dll")]
+        public static extern nint GetModuleHandle(string lpModuleName);
+
+        [DllImport("user32.dll")]
+        public static extern bool SetWindowPos(nint hWnd, nint hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
+    }
+}
